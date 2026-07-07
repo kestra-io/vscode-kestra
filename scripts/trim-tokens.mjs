72d09349db924ccfdde95aac98b1df6ef5ec0a7f
Malay Dewangan <malaydewangan310@gmail.com>
2026-07-07 13:13:09 +0530

    chore: drop the output trim step

--- scripts/trim-tokens.mjs
+++
@@ -1,5 +0,0 @@
-import fs from "node:fs";
-const file = "media/tokens.css";
-const css = fs.readFileSync(file, "utf8");
-const banner = css.indexOf("/* Generated from");
-fs.writeFileSync(file, css.slice(banner));
